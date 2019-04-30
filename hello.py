07a076f7856cfcb5a816b25878a65a6f41d31a9a
wenjchao <piggggg610@gmail.com>
2019-04-30 20:35:33 +0800

    save

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-a=[[1,2],[3,4]]
-print(a[1][1])
+while 'TRUE':
+    a = 0
+    print("請輸入五個數字")
+    num = list()
+    for i in  range(5):
+        temp = input("")
+        num.append(temp)
+    num.sort()
+    for j in range(4):
+        if num[j] == num[j+1]:
+            a = 1
+    if a == 1:
+        break
+    print("第二大的數是：", num[3] ,"第二小的數是：", num[1])
+print("你的數字重複了喔")
